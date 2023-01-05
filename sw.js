2ef9375a2c34dfa29290a4d60d2dafc377d390f0
Done Joseph <donejoseph95@gmail.com>
2023-01-05 20:30:20 +0530

    Updated sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
-// On install - caching the application shell
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add(["./index.html", "./css/style.css"]);
-    })
-  );
+const CACHE_NAME = 'cool-cache';
+
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
-// On network request
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
